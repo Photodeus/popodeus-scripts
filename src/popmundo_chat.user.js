82d4269520a7c6959315b3f239710623331cd594
Photodeus <photodeus+github@gmail.com>
2010-04-28 14:26:12 +0200

    Initial module created

---
+++ src/popmundo_chat.user.js
@@ -0,0 +1,15 @@
+// ==Userscript==
+// @name         Popmundo Chat
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// @copyright	 BSD-license
+// @version      0.1
+// ==/Userscript==
+
+(function() {
+
+	
+	// TODO 
+	// some funny code goes here
+	
+	
+});
