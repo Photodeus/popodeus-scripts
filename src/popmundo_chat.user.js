5966f405a6352af1f78df38db6a06e65a182aeb1
Photodeus <photodeus+github@gmail.com>
2010-04-28 14:52:23 +0200

    Script metadata added

--- src/popmundo_chat.user.js
+++ src/popmundo_chat.user.js
@@ -1,5 +1,7 @@
 // ==Userscript==
 // @name         Popmundo Chat
+// @description  A custom chat for Popmundo players integrated inside the game
+// @include      http://www*.popmundo.com/Common/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // @copyright	 BSD-license
 // @version      0.1
